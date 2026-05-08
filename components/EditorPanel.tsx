e7b69c6e95dcfcdf1654ab90f09c2eb1440be086
nicholasthefirst <166142239+nicholasthefirst@users.noreply.github.com>
2026-05-08 09:13:34 -0400

    Update EditorPanel.tsx

--- components/EditorPanel.tsx
+++ components/EditorPanel.tsx
@@ -1,19 +1,35 @@
 "use client";
 
 import Editor from "@monaco-editor/react";
-import { useState } from "react";
+import { io } from "socket.io-client";
 
-export default function EditorPanel() {
-  const [code, setCode] = useState(
-`console.log("Replit Remake running");`
-  );
+const socket = io("http://localhost:3001");
+
+type Props = {
+  code: string;
+  setCode: (v: string) => void;
+};
+
+export default function EditorPanel({ code, setCode }: Props) {
+  const run = () => {
+    socket.emit("run-code", code);
+  };
 
   return (
-    <div className="flex-1">
+    <div className="flex flex-col flex-1">
+      <div className="bg-gray-900 p-2">
+        <button
+          onClick={run}
+          className="bg-green-600 px-3 py-1 rounded"
+        >
+          ▶ Run
+        </button>
+      </div>
+
       <Editor
         height="100%"
         theme="vs-dark"
-        defaultLanguage="typescript"
+        language="typescript"
         value={code}
         onChange={(v) => setCode(v || "")}
       />
