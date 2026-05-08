a66af66156e2f713188d969f5de55cd2d2b5af3f
nicholasthefirst <166142239+nicholasthefirst@users.noreply.github.com>
2026-05-08 09:11:25 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,49 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import fs from "fs";
+import { exec } from "child_process";
 
 const httpServer = createServer();
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
-const io = new Server(httpServer, {
-  cors: { origin: "*" }
-});
+const PROJECT_DIR = "./projects";
+
+if (!fs.existsSync(PROJECT_DIR)) {
+  fs.mkdirSync(PROJECT_DIR);
+}
 
 io.on("connection", (socket) => {
-  console.log("user connected");
+  console.log("user connected:", socket.id);
 
-  socket.on("code-change", (data) => {
-    socket.broadcast.emit("code-update", data);
+  // RUN CODE
+  socket.on("run-code", (code: string) => {
+    const file = `${PROJECT_DIR}/main.ts`;
+    fs.writeFileSync(file, code);
+
+    exec(`npx ts-node ${file}`, (err, stdout, stderr) => {
+      socket.emit("terminal-output", stdout || stderr);
+    });
+  });
+
+  // SHELL COMMANDS
+  socket.on("shell", (cmd: string) => {
+    exec(cmd, { cwd: PROJECT_DIR }, (err, stdout, stderr) => {
+      socket.emit("terminal-output", stdout || stderr);
+    });
+  });
+
+  // FILE SYSTEM
+  socket.on("create-file", (name: string) => {
+    fs.writeFileSync(`${PROJECT_DIR}/${name}`, "");
+    socket.emit("file-update");
+  });
+
+  socket.on("list-files", () => {
+    const files = fs.readdirSync(PROJECT_DIR);
+    socket.emit("file-list", files);
   });
 });
 
-httpServer.listen(3001, () => {
-  console.log("Socket server running on 3001");
+httpServer.listen(3001, "0.0.0.0", () => {
+  console.log("Server running on 3001");
 });
